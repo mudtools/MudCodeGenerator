4646d21eefbb0fe50d35e9c8341aed671b71efb3
倔强的石头 <413901125@qq.com>
2025-10-11 13:43:22 +0800

    为示例代码的基类加上注释。

--- Test/CodeBaseTest/BaseEntity.cs
+++ Test/CodeBaseTest/BaseEntity.cs
@@ -4,6 +4,9 @@ namespace CodeBaseTest;
 
 public abstract class BaseEntity<TKey>
 {
+    /// <summary>
+    /// 实体主键
+    /// </summary>
     [Column(Name = "id", IsPrimary = true, Position = 1)]
     public TKey Id { get; set; }
 }
--- Test/CodeBaseTest/CRUDEntity.cs
+++ Test/CodeBaseTest/CRUDEntity.cs
@@ -4,15 +4,26 @@ namespace CodeBaseTest;
 
 public abstract class CRUDEntity : BaseEntity<string>
 {
+    /// <summary>
+    /// 对象创建人
+    /// </summary>
     [IgnoreGenerator]
     public long? CreatorId { get; set; }
 
-    [IgnoreGenerator]
+    /// <summary>
+    /// 对象创建时间
+    /// </summary>
     public DateTime? CreationTime { get; set; }
 
+    /// <summary>
+    /// 最后修改人
+    /// </summary>
     [IgnoreGenerator]
     public long? LastModifierId { get; set; }
 
+    /// <summary>
+    /// 最后修改时间
+    /// </summary>
     [IgnoreGenerator]
     public DateTime? LastModificationTime { get; set; }
 }
