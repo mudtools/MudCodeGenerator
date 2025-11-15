322bab3adbe0e2b9cc3c9bae5fa5e3f634be7b05
倔强的石头 <413901125@qq.com>
2025-11-15 09:41:14 +0800

    添加HTTP API包装特性。

---
+++ Test/Mud.Common.CodeGenerator/Http/HttpClientApiWrapAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace Mud.Common.CodeGenerator;
+
+[AttributeUsage(AttributeTargets.Interface, AllowMultiple = false)]
+public class HttpClientApiWrapAttribute : Attribute
+{
+    public HttpClientApiWrapAttribute()
+    {
+    }
+
+    public HttpClientApiWrapAttribute(string wrapInterface, string tokenManage)
+    {
+        WrapInterface = wrapInterface;
+        TokenManage = tokenManage;
+    }
+
+#if NET8_0_OR_GREATER
+    public required string WrapInterface { get; set; }
+
+    public required string TokenManage { get; set; }
+#else
+    public string WrapInterface { get; set; }
+
+    public string TokenManage { get; set; }
+#endif
+}
