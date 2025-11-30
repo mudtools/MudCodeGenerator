128be2712c48d1ef5ff9d326354d5539eee9e216
倔强的石头 <413901125@qq.com>
2025-11-30 22:50:06 +0800

    完善测试代码。

---
+++ Test/PublicPackageTest/WebApi/ITestTokenApi.cs
@@ -0,0 +1,59 @@
+namespace PublicPackageTest.WebApi;
+
+[HttpClientApi(TokenManage = nameof(ITokenManager), IsAbstract = true)]
+public interface ITestBaseTokenApi
+{
+    /// <summary>
+    /// 基类接口中获取用户信息
+    /// </summary>
+    [Get("api/users/{id}")]
+    Task<SysUserInfoOutput> GetBaeUserAsync([Path] string id);
+}
+
+/// <summary>
+/// 测试Token功能的API接口
+/// </summary>
+[HttpClientApi(TokenManage = nameof(ITokenManager), InheritedFrom = "TestBaseTokenApi")]
+[Header("Authorization", AliasAs = "X-Token")]
+[Header("xx1", "xxValue1")]
+[Header("xx2", "xxValue3")]
+public interface ITestNullTokenApi : ITestBaseTokenApi
+{
+
+}
+
+/// <summary>
+/// 测试Token功能的API接口
+/// </summary>
+[HttpClientApi(TokenManage = nameof(ITokenManager), InheritedFrom = "TestBaseTokenApi")]
+[Header("Authorization", AliasAs = "X-Token")]
+[Header("xx1", "xxValue1")]
+[Header("xx2", "xxValue3")]
+public interface ITestTokenApi : ITestBaseTokenApi
+{
+    /// <summary>
+    /// 获取用户信息
+    /// </summary>
+    [Get("api/users/{id}")]
+    Task<SysUserInfoOutput> GetUserAsync([Path] string id);
+
+    /// <summary>
+    /// 获取用户列表
+    /// </summary>
+    [Get("api/users")]
+    Task<List<SysUserInfoOutput>> GetUsersAsync();
+}
+
+/// <summary>
+/// 测试Query Authorization的API接口
+/// </summary>
+[HttpClientApi(TokenManage = nameof(ITokenManager))]
+[Query("Authorization", AliasAs = "X-Token")]
+public interface ITestTokenQueryApi
+{
+    /// <summary>
+    /// 获取用户信息（使用Query参数传递Token）
+    /// </summary>
+    [Get("api/users/{id}")]
+    Task<SysUserInfoOutput> GetUserAsync([Path] string id);
+}
